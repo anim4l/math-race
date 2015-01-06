a92546001b46b4fb2068ee240727a60b642d81d3
anim4l <puracchioj@gmail.com>
2015-01-06 19:54:38 -0300

    gral changes

---
+++ public/javascripts/evrythng.js
@@ -0,0 +1,14 @@
+product
+{
+  "product": "Ue46fMw4seKanrwUKkP4nfXd", 
+  "name": "{{ user.name }}", 
+  "identifiers": {"serial-number": "{{ user.name + user.id}}"},
+}
+
+thng
+{
+  "product": "Ue46fMw4seKanrwUKkP4nfXd", 
+  "name": "{{ user.name }}", 
+  "identifiers": {"id": "0000"},
+  "tags": ["uk", "whisky"]
+}
